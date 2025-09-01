3a71f6f65990e6ce558f6756f2320a8f122900b7
UtkarshxVerse <utkarsh2423@gmail.com>
2025-09-01 10:43:34 +0530

    first commit

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Context from './Context.jsx'
+import Context from './context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
